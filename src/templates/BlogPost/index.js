0ba4144739fb6e2759c4bd0c790cc576b1fa54e6
Daniel Vernon <danpvernon@gmail.com>
2019-01-03 14:05:37 +0000

    reorganise components

--- src/templates/BlogPost/index.js
+++ src/templates/BlogPost/index.js
@@ -3,9 +3,8 @@ import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import get from 'lodash/get'
 import Img from 'gatsby-image'
-import Layout from '../components/layout'
 
-import heroStyles from '../components/hero.module.css'
+import Layout from '../../components/Layout'
 
 class BlogPostTemplate extends React.Component {
   render() {
@@ -13,13 +12,13 @@ class BlogPostTemplate extends React.Component {
     const siteTitle = get(this.props, 'data.site.siteMetadata.title')
 
     return (
-      <Layout location={this.props.location} >
+      <Layout location={this.props.location}>
         <div style={{ background: '#fff' }}>
           <Helmet title={`${post.title} | ${siteTitle}`} />
-          <div className={heroStyles.hero}>
-          {post.heroImage &&
-            <Img className={heroStyles.heroImage} alt={post.title} fluid={post.heroImage.fluid} />
-          }
+          <div>
+            {post.heroImage && (
+              <Img alt={post.title} fluid={post.heroImage.fluid} />
+            )}
           </div>
           <div className="wrapper">
             <h1 className="section-headline">{post.title}</h1>
