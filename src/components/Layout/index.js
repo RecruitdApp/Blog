c44b29f5539265a526f8599ccb9e8ab039178f76
Daniel Vernon <danpvernon@gmail.com>
2019-01-04 14:45:55 +0000

    og + navigation links

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -24,7 +24,33 @@ class Template extends React.Component {
           link={[
             { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           ]}
-        />
+        >
+          <meta property="og:site_name" content="Recruitd Resources" />
+          <meta property="og:url" content="https://blog.recruitd.com" />
+          <meta property="og:title" content="Recruitd Blog" />
+          <meta property="og:type" content="website" />
+          <meta
+            property="og:description"
+            content="Whether securing your next job, hiring into to your organisation, or recruiting for a client, Recruitd is the easiest place to do it."
+          />
+          <meta
+            property="og:image"
+            content="https://s3.eu-west-2.amazonaws.com/alpha-recruitd/recruitd-banner.jpg"
+          />
+          <meta property="og:image:alt" content="Recruitd Blog" />
+          <meta name="twitter:card" content="summary_large_image" />
+          <meta name="twitter:creator" content="recruitdapp" />
+          <meta name="twitter:title" content="Recruitd Blog" />
+          <meta
+            name="twitter:description"
+            content="Whether securing your next job, hiring into to your organisation, or recruiting for a client, Recruitd is the easiest place to do it."
+          />
+          <meta
+            name="twitter:image"
+            content="https://s3.eu-west-2.amazonaws.com/alpha-recruitd/recruitd-banner.jpg"
+          />
+        </Helmet>
+
         <Navigation />
         <Container>{children}</Container>
         <Footer />
