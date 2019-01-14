c40c017f53caae2605d22e872dd49adf3a18c819
Daniel Vernon <danpvernon@gmail.com>
2019-01-14 14:13:46 +0000

    fixes 4

--- src/templates/BlogPost/index.js
+++ src/templates/BlogPost/index.js
@@ -17,7 +17,7 @@ class BlogPostTemplate extends React.Component {
     const shareUrl = 'https://blog.recruitd.com/'
     const twitterHandle = '@recruitdapp'
     const shareTitle = post.title
-    const socialImage = post.socialImage.fluid.src
+    const socialImage = post.socialImage.fluid
       ? post.socialImage.fluid.src
       : null
 
